553eab1f0c3d813bc8accdeafdb4a00d1dce304f
skirky3605 <qofnajgm@126.com>
2026-06-22 09:33:15 +0800

    添加项目文件。

---
+++ ExplorerRevolution/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using Mile.Xaml;
+using MileXamlBlankAppNetFrameworkModern;
+using System;
+using System.Windows.Forms;
+using Windows.UI.Xaml;
+
+namespace MileXamlBlankAppNetFramework
+{
+    public partial class MainForm : Form
+    {
+        WindowsXamlHost xamlHost = new WindowsXamlHost();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            
+            this.Controls.Add(xamlHost);
+            xamlHost.AutoSize = true;
+            xamlHost.Dock = DockStyle.Fill;
+            xamlHost.Child = new MainPage();
+
+            this.Load += Form1_Load;
+
+            //ContainerControl control = new ContainerControl();
+            //this.Controls.Add(control);
+            //control.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
