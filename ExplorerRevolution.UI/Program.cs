62b25f7d172ee35e2f680a89a556116492abee69
skirky3605 <qofnajgm@126.com>
2026-06-23 17:36:57 +0800

    添加多窗口，将窗口移至正确位置 格式化xaml文档 清理代码

--- ExplorerRevolution.UI/Program.cs
+++ ExplorerRevolution.UI/Program.cs
@@ -3,6 +3,7 @@ using ExplorerRevolution.UI;
 using Mile.Xaml;
 using System;
 using System.Drawing;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Windows.UI.Xaml;
 using static ExplorerRevolution.Common.NativeMethods;
@@ -12,8 +13,6 @@ namespace ExplorerRevolution
 {
     public static class Program
     {
-        public static Form MainForm { get; private set; }
-
         [STAThread]
         private static void Main()
         {
@@ -22,39 +21,7 @@ namespace ExplorerRevolution
 
             App app = new();
 
-            var DesktopForm = new Form();
-            DesktopForm.FormBorderStyle = FormBorderStyle.None;
-            DesktopForm.WindowState = FormWindowState.Normal;
-            DesktopForm.Bounds = Screen.PrimaryScreen.Bounds;
-
-            DesktopForm.AllowTransparency = true;
-            DesktopForm.BackColor = Color.LimeGreen;
-            DesktopForm.TransparencyKey = Color.LimeGreen;
-            DesktopForm.TopMost = false;
-            Common.HookExplorer.HideExplorer();
-            Common.HookExplorer.AttachToWorkerW(DesktopForm.Handle);
-            WindowsXamlHost DesktopXamlHost = new WindowsXamlHost();
-            DesktopForm.Controls.Add(DesktopXamlHost);
-            DesktopXamlHost.AutoSize = true;
-            DesktopXamlHost.Dock = DockStyle.Fill;
-            DesktopXamlHost.Child = new DesktopPage();
-            Application.Run(DesktopForm);
-
-            MainForm = new MainForm();
-            #region 替代explorer
-            MainForm.FormBorderStyle = FormBorderStyle.None;
-            MainForm.WindowState = FormWindowState.Normal;
-            MainForm.Bounds = Screen.PrimaryScreen.Bounds;
-
-            MainForm.AllowTransparency = true;
-            MainForm.BackColor = Color.LimeGreen;
-            MainForm.TransparencyKey = Color.LimeGreen;
-            MainForm.TopMost = false;
-            Common.HookExplorer.HideExplorer();
-            Common.HookExplorer.AttachToWorkerW(MainForm.Handle);
-            #endregion
-
-            Application.Run(MainForm);
+            Application.Run(new ShellContext());
             AppDomain.CurrentDomain.ProcessExit += (s, e) =>
             {
                 HookExplorer.RestoreExplorer();
---
+++ ExplorerRevolution.UI/ShellContext.cs
@@ -0,0 +1,88 @@
+﻿using Mile.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExplorerRevolution.UI
+{
+    public class ShellContext : ApplicationContext
+    {
+        public Form DesktopForm { get; }
+        public Form TaskBarForm { get; }
+
+        public ShellContext()
+        {
+            Common.HookExplorer.HideExplorer();
+            DesktopForm = CreateDesktopForm();
+            MainForm = CreateTaskBarForm();
+
+            DesktopForm.FormClosed += OnFormClosed;
+            MainForm.FormClosed += OnFormClosed;
+
+            DesktopForm.Show();
+            MainForm.Show();
+        }
+
+        private void OnFormClosed(object? sender, FormClosedEventArgs e)
+        {
+            if (DesktopForm.IsDisposed && MainForm.IsDisposed)
+            {
+                ExitThread();
+            }
+        }
+
+        private Form CreateDesktopForm()
+        {
+            var DesktopForm = new Form();
+            DesktopForm.FormBorderStyle = FormBorderStyle.None;
+            DesktopForm.Bounds = Screen.PrimaryScreen.Bounds;
+            DesktopForm.BackColor = Color.LimeGreen;
+            DesktopForm.TransparencyKey = Color.LimeGreen;
+
+            Common.HookExplorer.AttachToWorkerW(DesktopForm.Handle);
+
+            WindowsXamlHost DesktopXamlHost = new WindowsXamlHost();
+            DesktopForm.Controls.Add(DesktopXamlHost);
+            DesktopXamlHost.AutoSize = true;
+            DesktopXamlHost.Dock = DockStyle.Fill;
+            DesktopXamlHost.Child = new DesktopPage();
+
+            return DesktopForm;
+        }
+
+        private Form CreateTaskBarForm()
+        {
+            var TaskBarForm = new Form();
+            TaskBarForm.FormBorderStyle = FormBorderStyle.None;
+            TaskBarForm.BackColor = Color.LimeGreen;
+            TaskBarForm.TransparencyKey = Color.LimeGreen;
+            TaskBarForm.TopMost = true;
+
+            TaskBarForm.Show();
+
+            using var g = TaskBarForm.CreateGraphics();
+            float dpiScale = g.DpiX / 96f;
+
+            int taskBarHeight = (int)(48 * dpiScale);
+
+            var screen = Screen.FromControl(TaskBarForm).Bounds;
+
+            TaskBarForm.Width = screen.Width;
+            TaskBarForm.Height = taskBarHeight;
+            TaskBarForm.Left = screen.Left;
+            TaskBarForm.Top = screen.Bottom - taskBarHeight;
+
+            WindowsXamlHost TaskBarXamlHost = new WindowsXamlHost();
+            TaskBarForm.Controls.Add(TaskBarXamlHost);
+            TaskBarXamlHost.AutoSize = false;
+            TaskBarXamlHost.Dock = DockStyle.Fill;
+            TaskBarXamlHost.Child = new TaskBar();
+
+            return TaskBarForm;
+        }
+    }
+}
